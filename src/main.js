635c71fc34408319c23979e44038d72e34a9b62e
Joe'Gu <sg5736@nyu.edu>
2023-12-28 21:34:00 -0800

    remove md-editor

--- src/main.js
+++ src/main.js
@@ -2,19 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import VueMarkdownEditor from "@kangc/v-md-editor";
-import "@kangc/v-md-editor/lib/style/base-editor.css";
-import vuepressTheme from "@kangc/v-md-editor/lib/theme/vuepress.js";
-import "@kangc/v-md-editor/lib/theme/style/vuepress.css";
-
-import Prism from "prismjs";
-  
-
-VueMarkdownEditor.use(vuepressTheme, {
-  Prism,
-});
-
 createApp(App)
-.use(VueMarkdownEditor)
 .use(router)
 .mount("#app");
